c9622633def32ee97bec9b498738a185ec6b1622
Scott Stanchfield <scott@javadude.com>
2013-11-28 14:29:19 -0500

    fix template

---
+++ tools/boxbeans/images/_vti_cnf/marvon.php
@@ -0,0 +1,14 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|28 Nov 2013 03:23:21 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|CARTMAN\\Scott
+vti_modifiedby:SR|CARTMAN\\Scott
+vti_timecreated:TR|28 Nov 2013 03:23:21 -0000
+vti_cacheddtm:TX|28 Nov 2013 03:23:21 -0000
+vti_filesize:IR|19491
+vti_cachedneedsrewrite:BR|false
+vti_cachedhasbots:BR|false
+vti_cachedhastheme:BR|false
+vti_cachedhasborder:BR|false
+vti_charset:SR|utf-8
+vti_backlinkinfo:VX|
