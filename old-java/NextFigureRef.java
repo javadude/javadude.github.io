40e1c18fb9ea0baaec2fab81c727dcd21facc121
Scott Stanchfield <scott@javadude.com>
2013-11-27 22:50:37 -0500

    commit 3

---
+++ old-java/NextFigureRef.java
@@ -0,0 +1,9 @@
+package com.javadude.javamage;
+
+public class NextFigureRef extends FigureRef {
+	private static final long serialVersionUID = 1L;
+
+	public NextFigureRef() {
+		super(true);
+	}
+}
---
+++ old-java/PrevFigureRef.java
@@ -0,0 +1,8 @@
+package com.javadude.javamage;
+
+public class PrevFigureRef extends FigureRef {
+	private static final long serialVersionUID = 1L;
+	public PrevFigureRef() {
+		super(false);
+	}
+}
