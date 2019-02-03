5796f748ddd4da865ce0f775bc3850a95ad7490e
Scott Stanchfield <scott@javadude.com>
2019-02-02 23:26:17 -0500

    New Website format - first attempt

--- articles/layouts/_vti_cnf/FileDisplay1.java
+++
@@ -1,9 +0,0 @@
-vti_encoding:SR|utf8-nl
-vti_author:SR|CARTMAN\\Scott
-vti_modifiedby:SR|CARTMAN\\Scott
-vti_timelastmodified:TR|28 Nov 2013 19:55:01 -0000
-vti_timecreated:TR|28 Nov 2013 19:55:01 -0000
-vti_cacheddtm:TX|28 Nov 2013 19:55:01 -0000
-vti_filesize:IR|5480
-vti_extenderversion:SR|12.0.0.0
-vti_backlinkinfo:VX|articles/layouts/index.html
--- old-java/NextFigureRef.java
+++
@@ -1,9 +0,0 @@
-package com.javadude.javamage;
-
-public class NextFigureRef extends FigureRef {
-	private static final long serialVersionUID = 1L;
-
-	public NextFigureRef() {
-		super(true);
-	}
-}
